9b367ea82c8b9615795911b9e59c6deb165b17a6
Alejo López <alejolo311@gmail.com>
2020-03-21 21:34:39 +0000

    configurations to set dev enviroment or production enviroment

---
+++ config/config.js
@@ -0,0 +1,39 @@
+/*
+ * Parser: this function is in charge of parse all the info present in the request
+ * and store the results in a objetc
+ * req: the arg of the func is the full request of the server.
+ * Return: returns a object with the parsed-data.
+ * Author: Alejo López
+ */
+
+// Container for all environments
+var environments = {};
+
+// Staging (default) environment
+environments.staging = {
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: "staging"
+};
+
+// Production environment
+environments.production = {
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: "production"
+};
+
+// Determine which environment was passed as a command-line argument
+let currentEnvironment =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLowerCase()
+    : "";
+
+// Check that the current environment is one of the environments above, if not default to staging
+let environmentToExport =
+  typeof environments[currentEnvironment] == "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
