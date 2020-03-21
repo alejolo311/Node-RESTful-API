b86c9999b350b9d9adafa77d2b00b9dbbf83bd22
Alejo López <alejolo311@gmail.com>
2020-03-21 16:16:05 +0000

    adding the logic to parse HTTP methods, Querystrings, Headers and Payloads

--- index.js
+++ index.js
@@ -7,18 +7,38 @@
 // Dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // Setting the response of the server
 let server = http.createServer((req, res) => {
   // Parse the url
   let parsedUrl = url.parse(req.url, true);
+
   // Get the path
   let path = parsedUrl.pathname;
   let trimmedPath = path.replace(/^\/+|\/+$/g, "");
-  // Send the response
-  res.end("Hello world \n");
-  // Log the path
-  console.log(`Request is recieved in path ${trimmedPath}`);
+
+  // Ge the Querystring
+  let queryString = parsedUrl.query;
+
+  // Get the HTTP method
+  let method = req.method.toLowerCase();
+
+  // Get the headers
+  let headers = req.headers;
+
+  // get the Payload
+  let decoder = new StringDecoder("utf-8");
+  let payloadBuffer = "";
+  req.on("data", data => {
+    payloadBuffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    payloadBuffer += decoder.end();
+
+    // Send the response
+    res.end("Hello world \n");
+  });
 });
 
 // Starting the server on port 3000
