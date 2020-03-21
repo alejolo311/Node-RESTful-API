e449cc11119f00ceb1052ad523efdf3a9f315e45
Alejo López <alejolo311@gmail.com>
2020-03-21 21:35:14 +0000

    adding the service in HTTPS and move some logic to router function

--- index.js
+++ index.js
@@ -6,40 +6,38 @@
 
 // Dependencies
 const http = require("http");
-const { parse } = require("./parser");
-const { router } = require("./router");
+const https = require("https");
+const { parse } = require("./helpers/parser");
+const { router } = require("./helpers/router");
+const config = require("./config/config.js");
+const fs = require("fs");
 
-// Setting the response of the server
-let server = http.createServer((req, res) => {
+// instance of HTTP server
+let httpServer = http.createServer((req, res) => {
   // Parse all the request and store it in a object
   let data = parse(req);
 
-  // Send the path to the router to obtain the correct handler for each case
-  let handler = router(data.trimmedPath);
-
-  // Call the handler to execute the action required by te user
-  handler(data, (statusCode, payload) => {
-    // Check if the handler return the status code
-    statusCode = typeof statusCode == "number" ? statusCode : 200;
-
-    // Check if the handler return the payload
-    payload = typeof payload == "object" ? payload : {};
-
-    // Convert the payload-object in a string
-    let payloadString = JSON.stringify(payload);
-
-    // Set the content type header in json format
-    res.setHeader("Content-Type", "application/json");
-
-    // Reponse the status code
-    res.writeHead(statusCode);
-
-    // Response to the client
-    res.end(payloadString);
-  });
+  // Send the path and the data to the router
+  router(data, res);
+});
+// Starting the HTTP server
+httpServer.listen(config.httpPort, () => {
+  console.log(`the server is listening on port ${config.httpPort}`);
 });
 
-// Starting the server on port 3000
-server.listen(3000, () => {
-  console.log("the server is listening on port 3000");
+// instance of HTTPS server
+let httpsServerOptions = {
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem")
+};
+let httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+  // Parse all the request and store it in a object
+  let data = parse(req);
+
+  // Send the path and the data to the router
+  router(data, res);
+});
+// Starting the HTTPS server
+httpsServer.listen(config.httpsPort, () => {
+  console.log(`the server is listening on port ${config.httpsPort}`);
 });
